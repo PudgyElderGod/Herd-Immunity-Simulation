5f46bbf3e412a21b76299769dd9cf016176029f1
PudgyElderGod <PudgyElderGod@gmail.com>
2019-10-05 13:20:12 -0700

    Added code to log_interaction

--- logger.py
+++ logger.py
@@ -1,3 +1,4 @@
+#---------Logger Class----------#
 class Logger(object):
     ''' Utility class responsible for logging all interactions during the simulation. '''
     def __init__(self, file_name):
@@ -35,7 +36,9 @@ class Logger(object):
         # represent all the possible edge cases. Use the values passed along with each person,
         # along with whether they are sick or vaccinated when they interact to determine
         # exactly what happened in the interaction and create a String, and write to your logfile.
-        pass
+        if random_person_sick = true:
+            did_infect = True
+            return did_infect
 
     def log_infection_survival(self, person, did_die_from_infection):
         ''' The Simulation object uses this method to log the results of every
